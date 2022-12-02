7814539a58669d694582c55a6087f828951cec95
Gimenez Vano, Aleix <agvano@netsuite.com>
2022-12-02 12:18:17 +0100

    Second Day

--- src/Solutions.ts
+++ src/Solutions.ts
@@ -1,6 +1,7 @@
 import {Solution} from "./Solution";
 import {day01} from "./day01/CalorieCounting";
+import {day02} from "./day02/RockPaperScissors";
 
 export function getSolutions(): Solution[] {
-    return [day01]
+    return [day01, day02]
 }
