bb53b9128eebc8f7981f94bb7ff1b8562237d94d
Gimenez Vano, Aleix <agvano@netsuite.com>
2022-12-12 18:20:22 +0100

    Third Day

--- src/Solutions.ts
+++ src/Solutions.ts
@@ -1,7 +1,8 @@
 import {Solution} from "./types";
 import {day01} from "./day01/CalorieCounting";
 import {day02} from "./day02/RockPaperScissors";
+import {day03} from "./day03/RucksackReorganization";
 
 export function getSolutions(): Solution[] {
-    return [day01, day02]
+    return [day01, day02, day03]
 }
